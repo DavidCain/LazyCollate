45a2ff8460fdbd202b7ea2b8532151c23e38c4bb
David Cain <davidjosephcain@gmail.com>
2013-02-10 16:26:26 -0500

    Add classes to carry out main collating tasks

--- collate.py
+++ collate.py
@@ -1,46 +1,119 @@
+#!/usr/bin/env python
+# David Cain
+# 2013-02-08
+
+""" A script to automatically collate projects for CS151 students.
+
+Dependencies: wkhtmltopdf (for automated printing of writeups)
+"""
+
 import os
 import re
 import shutil
+import subprocess
 
+
+SEMESTER, YEAR = "s", 13  # s for spring, f for fall
 CS151_MOUNT_POINT = "/mnt/CS151"
 COLLATED_DIR = "/mnt/CS151/Collated/"
+PROJ_REGEX = "^proj(ect)?[_\s]*0*%d$"  # substitute project number
+
+
+class AbsentWriteup(Exception):
+    pass
+
+
+class AbsentProject(Exception):
+    pass
+
+
+class Project(object):
+    def __init__(self, proj_num):
+        self.proj_regex = re.compile(PROJ_REGEX % proj_num, re.IGNORECASE)
+        self.wiki_label = "cs151%s%dproject%d" % (SEMESTER, YEAR, proj_num)
+        self.proj_num = proj_num
+
+    def match(self, dirname):
+        return self.proj_regex.match(os.path.split(dirname)[1])
 
 
 class Collate(object):
     def __init__(self, proj_num):
-        self.proj_num = proj_num
-        self.proj_regex = re.compile("^proj(ect)?[_\s]*0*%d$" % proj_num, re.IGNORECASE)
+        self.project = Project(proj_num)
         self.collated_proj_dir = os.path.join(COLLATED_DIR,
-                                              "Proj%d" % self.proj_num)
+                                              "Proj%d" % proj_num)
+        self.reset_collated_dir()
+
+    def reset_collated_dir(self):
         if os.path.isdir(self.collated_proj_dir):
             shutil.rmtree(self.collated_proj_dir)
         os.mkdir(self.collated_proj_dir)
 
-    def get_proj_dir(self, colby_id):
-        stu_dir = os.path.join(CS151_MOUNT_POINT, colby_id, "private")  # TODO: don't delve into private
-        os.chdir(stu_dir)
+    def collate_projects(self, students):
+        for colby_id in students:
+            stu = StudentCollate(colby_id, self.project, self.collated_proj_dir)
+            stu.collect()
+            stu.get_writeup(None)
 
-        all_dirs = [path for path in os.listdir(stu_dir) if os.path.isdir(path)]
-        matching_dirs = [d for d in all_dirs if self.proj_regex.match(d)]
 
-        # Return matching directory if normal, otherwise handle cases
-        if len(matching_dirs) == 1:
-            return matching_dirs[0]
-        elif len(matching_dirs) > 1:
-            raise Exception("Ambiguous which is project: %s" % matching_dirs)
-        else:
-            print "No project found for '%s'." % colby_id
+class StudentCollate(object):
+    def __init__(self, colby_id, project, collated_out_dir):
+        self.colby_id = colby_id
+        self.project = project
+        self.stu_dir = os.path.join(CS151_MOUNT_POINT, self.colby_id)
+        self.private_dir = os.path.join(self.stu_dir, "private")
+        self.collated_dest = os.path.join(collated_out_dir, self.colby_id)
 
-    def collect_student(self, colby_id):
-        collated_dest = os.path.join(self.collated_proj_dir, colby_id)
-        if os.path.isdir(collated_dest):
-            print "Removing existing directory '%s'" % collated_dest
-            shutil.rmtree(collated_dest)
+    def collect(self):
+        """ Collect student's code into the collated directory. """
+        if os.path.isdir(self.collated_dest):
+            print "Removing existing directory '%s'" % self.collated_dest
+            shutil.rmtree(self.collated_dest)
 
-        proj_dir = self.get_proj_dir(colby_id)
-        if not proj_dir:
-            return
+        try:
+            proj_dir = self.get_proj_dir()
+        except ValueError as e:
+            print e.message
+            # TODO: resolve interactivity?
+            print "Resolve which project directory applies"
+            raise
 
-        shutil.copytree(proj_dir, collated_dest)
+        shutil.copytree(proj_dir, self.collated_dest)
 
-        # TODO: Go to their wiki, print page
+    def get_proj_dir(self):
+        """ Return a directory where code in unambiguously published.
+
+        Will try to publish private version if possible.
+        """
+        # Find all matching directories in top level, then private if need be
+        matching_dirs = self._get_matching_dirs(self.stu_dir)
+        if not matching_dirs:
+            matching_dirs = self._get_matching_dirs(self.private_dir)
+            if not matching_dirs:
+                raise AbsentProject("No project found for '%s'." % self.colby_id)
+            if len(matching_dirs) == 1:
+                print "No published project for '%s', using private version." % self.colby_id
+
+        if len(matching_dirs) > 1:
+            raise ValueError("Ambiguous which is project: %s" % matching_dirs)
+        return matching_dirs[0]
+
+        # Return matching directory if found, raise Exceptions if absent
+
+    def _get_matching_dirs(self, search_dir):
+        """ Search for a project in the top level of the given directory. """
+        # Obtain all matching directories (hopefully, just one matches)
+        init_dir = os.getcwd()
+        os.chdir(search_dir)
+        all_dirs = [path for path in os.listdir(search_dir) if os.path.isdir(path)]
+        matching_dirs = [d for d in all_dirs if self.project.match(d)]
+        abs_dirs = map(os.path.abspath, matching_dirs)
+        os.chdir(init_dir)
+
+        return abs_dirs
+
+    def get_writeup(self, writeup_url):
+        print self.project.wiki_label
+        dest_pdf = os.path.join(self.collated_dest, "writeup.pdf")
+        subprocess.check_call(["wkhtmltopdf", "--quiet", writeup_url, dest_pdf])
+        return dest_pdf

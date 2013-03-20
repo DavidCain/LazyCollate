b8e7db898efcf35bdab61b802b727f8d256d5f67
David Cain <davidjosephcain@gmail.com>
2013-03-20 01:59:09 -0400

    Implement detection of failed logins

--- writeups.py
+++ writeups.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
+
 # David Cain
-# 2013-02-11
+# 2013-03-20
 
 """ Fetch all writeups for a project and map writeups to users. """
 
@@ -8,6 +9,12 @@ from collections import defaultdict
 import getpass
 
 import mechanize
+
+# Strings used by the Confluence system in the HTML for failed login attempts.
+# These apply to Confluence 3.5.13 (might need to be updated in later versions).
+# If they don't match Confluence errors, failed logins will pass silently
+FAILURE_MSG = "Please try again"
+CAPTCHA_MSG = "validate your login"
 
 
 class PageFetch(object):
@@ -20,23 +27,42 @@ class PageFetch(object):
         self.browser = mechanize.Browser()
         self.cj = mechanize.MozillaCookieJar()
         self.browser.set_cookiejar(self.cj)
-        self.login(cookie_jar, password, force_login)
+        self.cookie_login(cookie_jar, password, force_login)
 
-    def login(self, cookie_jar, password=None, force_login=True):
-        """ Log into Confluence, save the cookie. """
+    def cookie_login(self, cookie_jar, password=None, force_login=True):
+        """ Log in if necessary, saving the resulting cookie. """
         if password or force_login:
-            self.browser.open(self.login_url)
-
-            self.browser.select_form(name="loginform")
-            self.browser["os_username"] = self.collator
-            if not password:
-                password = getpass.getpass("Colby password: ")
-            self.browser["os_password"] = password
-
-            results = self.browser.submit()  # TODO: no verification that successful
+            self.login(password)
             self.cj.save(cookie_jar, ignore_discard=True, ignore_expires=True)
         else:  # Load a cookie from file
+            # NOTE: this should work, but doesn't seem to
             self.cj.load(cookie_jar, ignore_discard=True, ignore_expires=True)
+
+    def login(self, password=None):
+        """Repeat the login process until successfully logged in. """
+        if not password:
+            password = getpass.getpass("Colby password: ")
+        while True:
+            login_response = self._submit_credentials(password)
+            response_lines = login_response.readlines()
+            if any(FAILURE_MSG in line for line in response_lines):
+                print "Login failed. Please try again"
+                password = getpass.getpass("Colby password: ")
+            elif any(CAPTCHA_MSG in line for line in response_lines):
+                raise Exception("Login requires CAPTCHA! Manually log in at "
+                                "%s and re-run to fix" % self.login_url)
+            else:
+                break  # Login succesful
+
+    def _submit_credentials(self, password):
+        """ Submit login and password to Confluence, return page contents. """
+        self.browser.open(self.login_url)
+
+        self.browser.select_form(name="loginform")
+        self.browser["os_username"] = self.collator
+        self.browser["os_password"] = password
+
+        return self.browser.submit()
 
     def get_all_writeups(self):
         """ Starting on a page with writeups, yield all pages until exhausted. """
